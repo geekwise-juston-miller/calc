09a7d1cce1c136d4611f771801dff0b7407976ad
Juston Miller <geekwise.juston.miller@gmail.com>
2016-04-15 17:04:37 +0000

    todays update

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// function button_add(){
+//     document.Calculator.display.valueOf() + "+";
+//     document.Calculator.display.style.textAlign = "right";
+    
+// };
+
+// function button_subtrat(){
+//     document.Calculator.display.valueOf() + "-";
+//     document.Calculator.display.style.textAlign = "right";
+    
+// };
+// function button_clear(){
+//     document.Calculator.display.valueOf() + "";
+//     document.Calculator.display.style.textAlign = "right";
+    
+// };
+
+document.addEventListener("DOMContentLoaded", function(event){
+    
+    function button_add(){
+    document.Calculator.display.value() + "+";
+    document.Calculator.display.style.textAlign = "right";
+    
+};
+
+function button_subtrat(){
+    document.Calculator.display.value() + "-";
+    document.Calculator.display.style.textAlign = "right";
+    
+};
+function button_clear(){
+    document.Calculator.display.value() + "";
+    document.Calculator.display.style.textAlign = "right";
+    
+};
+    
+    
+});
