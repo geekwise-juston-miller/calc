a53375b18750b3087d39dd8cbc56955dce74b55e
Juston Miller <geekwise.juston.miller@gmail.com>
2016-04-21 17:49:36 +0000

    start of Calc

--- js/script.js
+++ js/script.js
@@ -1,38 +1,39 @@
-// function button_add(){
-//     document.Calculator.display.valueOf() + "+";
-//     document.Calculator.display.style.textAlign = "right";
-    
-// };
+var calc_numbers = [1,2,3,4,'+','-','='];
 
-// function button_subtrat(){
-//     document.Calculator.display.valueOf() + "-";
-//     document.Calculator.display.style.textAlign = "right";
-    
-// };
-// function button_clear(){
-//     document.Calculator.display.valueOf() + "";
-//     document.Calculator.display.style.textAlign = "right";
-    
-// };
+var calc_container;
+
+var answer_display;
+
+calc_container = document.createElement("div");
+calc_container.setAttribute("id", "my_calc_container");
+calc_container.style.width = "200px";
+calc_container.style.height = "100px";
+calc_container.style.backgroundColor = "yellow";
+
+
+var create_element = function(element_type, element_id){
+    var element = document.createElement(element_type);
+    element.setAttribute( 'id', element_id);
+    calc_container.appendChild(element);
+   
+
+};
+
 
 document.addEventListener("DOMContentLoaded", function(event){
     
-    function button_add(){
-    document.Calculator.display.value() + "+";
-    document.Calculator.display.style.textAlign = "right";
+    create_element("input", "answer_display");
     
-};
-
-function button_subtrat(){
-    document.Calculator.display.value() + "-";
-    document.Calculator.display.style.textAlign = "right";
+     document.body.appendChild(calc_container);
+    for( var i = 0; i < calc_numbers.length; i++){
+        create_element("button", "calc_numbers_" +i);
     
-};
-function button_clear(){
-    document.Calculator.display.value() + "";
-    document.Calculator.display.style.textAlign = "right";
-    
-};
-    
+    // var create_element = document.getElementById("calc_numbers" + i);
+    //     current_element.addEventListener("onclick", function(event){
+            
+    //     answer_display.value = answer_display.value += this.textContent;
+    //     });
+        
+    }
     
 });
